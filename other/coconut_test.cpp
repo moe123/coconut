d73ad34944e237ad9a669480df78cf5d14e4ea47
cucurbita <uu0mmw@gmail.com>
2016-02-22 14:03:19 -0800

    KVC binding selkey '@self'

--- other/coconut_test.cpp
+++ other/coconut_test.cpp
@@ -1017,6 +1017,17 @@ static void run_queue(void)
 
 int main(int argc, const char * argv[])
 {
+	String hello = u8"hello";
+	
+	// public API to be considered off limit
+	auto self = hello.valueForKey(u8"@self");
+	
+	std::cerr << "+ self : " << self << std::endl;
+	
+	self = hello.valueForKey(u8"@self.@stringValue");
+	
+	std::cerr << "+ self : " << self << std::endl;
+	
 	//test_stuff();
 	String s_0 = u"\u00f6";
 	String s_1 = u"o\u0308";
