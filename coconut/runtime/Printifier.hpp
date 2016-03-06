716907f981b778b4ec7c085aef77c3252e7114cc
cucurbita <uu0mmw@gmail.com>
2016-03-05 20:16:07 -0800

    recursive Printifier

--- coconut/runtime/Printifier.hpp
+++ coconut/runtime/Printifier.hpp
@@ -60,7 +60,7 @@ namespace coconut
 			os << '{';
 			for(auto it = ref_cast<Dictionary>(r).cbegin(); it != ref_cast<Dictionary>(r).cend(); ++it) {
 				if ((*it).first && (*it).second) {
-					os << (*it).first << ':' << (*it).second; if(std::next(it) != ref_cast<Dictionary>(r).cend()) { os << ',' << ' '; }
+					os << (*it).first << ':' << ' ' << (*it).second; if(std::next(it) != ref_cast<Dictionary>(r).cend()) { os << ',' << ' '; }
 				}
 			}
 			os << '}';
@@ -121,7 +121,7 @@ namespace coconut
 				os << '{';
 				for(auto it = ptr_cast<Dictionary>(r)->cbegin(); it != ptr_cast<Dictionary>(r)->cend(); ++it) {
 					if ((*it).first && (*it).second) {
-						os << (*it).first << ':' << (*it).second; if(std::next(it) != ptr_cast<Dictionary>(r)->cend()) { os << ',' << ' '; }
+						os << (*it).first << ':' << ' ' << (*it).second; if(std::next(it) != ptr_cast<Dictionary>(r)->cend()) { os << ',' << ' '; }
 					}
 				}
 				os << '}';
@@ -189,7 +189,7 @@ namespace coconut
 		os << '{';
 		for(auto it = r.cbegin(); it != r.cend(); ++it) {
 			if ((*it).first && (*it).second) {
-				os << (*it).first << ':' << (*it).second; if(std::next(it) != r.cend()) { os << ',' << ' '; }
+				os << (*it).first << ':' << ' ' << (*it).second; if(std::next(it) != r.cend()) { os << ',' << ' '; }
 			}
 		}
 		os << '}';
@@ -208,65 +208,12 @@ namespace coconut
 		os << '{';
 		for(auto it = r->cbegin(); it != r->cend(); ++it) {
 			if ((*it).first && (*it).second) {
-				os << (*it).first << ':' << (*it).second; if(std::next(it) != r->cend()) { os << ',' << ' '; }
+				os << (*it).first << ':' << ' ' << (*it).second; if(std::next(it) != r->cend()) { os << ',' << ' '; }
 			}
 		}
 		os << '}';
 		return os;
 	}
-			
-#if 0
-	template<typename TypeT, template<typename, typename...> class CollT, typename... Args,
-		typename std::enable_if<
-			std::is_same<TypeT,
-				std::deque<
-					typename TypeT::value_type,
-					typename TypeT::allocator_type
-				>
-			>::value ||
-			std::is_same<TypeT,
-				std::forward_list<
-					typename TypeT::value_type,
-					typename TypeT::allocator_type
-				>
-			>::value ||
-			std::is_same<TypeT,
-				std::list<
-					typename TypeT::value_type,
-					typename TypeT::allocator_type
-				>
-			>::value ||
-			std::is_same<TypeT,
-				std::set<
-					typename TypeT::value_type,
-					typename TypeT::allocator_type
-				>
-			>::value ||
-			std::is_same<TypeT,
-				std::unordered_set<
-					typename TypeT::value_type,
-					typename TypeT::allocator_type
-				>
-			>::value ||
-			std::is_same<TypeT,
-				std::vector<
-					typename TypeT::value_type,
-					typename TypeT::allocator_type
-				>
-			>::value
-		>::type* = nullptr
-	>
-	inline auto operator << (std::ostream & os, const CollT<TypeT, Args...> & r)
-		-> std::ostream &
-	{
-		os << '[';
-		for(auto it = r.cbegin(); it != r.cend(); ++it) {
-			os << *it; if(std::next(it) != r.cend()) { os << ',' << ' '; }
-		}
-		os << ']';
-		return os;
-	}
-#endif
 }
 
 #endif /* !COCONUT_RUNTIME_TO_FOUNDATION_PRINTIFIER_HPP */
