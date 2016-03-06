f90908e631c079f79111f5d60e4c0b45c0db3ff9
cucurbita <uu0mmw@gmail.com>
2016-03-06 13:48:57 -0800

    recursive Printifier

--- coconut/runtime/Printifier.hpp
+++ coconut/runtime/Printifier.hpp
@@ -14,6 +14,13 @@ namespace coconut
 	template <typename TypeT,
 		typename std::enable_if<
 			std::is_base_of<Any, TypeT>::value &&
+			!std::is_same<TypeT, String>::value &&
+			!std::is_same<TypeT, Date>::value &&
+			!std::is_same<TypeT, Data>::value &&
+			!std::is_same<TypeT, Path>::value &&
+			!std::is_same<TypeT, MutablePath>::value &&
+			!std::is_same<TypeT, URL>::value &&
+			!std::is_same<TypeT, Number>::value &&
 			!std::is_same<TypeT, Array>::value &&
 			!std::is_same<TypeT, MutableArray>::value &&
 			!std::is_same<TypeT, OrderedSet>::value &&
@@ -27,25 +34,26 @@ namespace coconut
 	inline auto operator << (std::ostream & os, const TypeT & r)
 		-> std::ostream &
 	{
-		if (r.isKindOf(StringClass) || r.isKindOf(PathClass) || r.isKindOf(URLClass) || r.isKindOf(DateClass)) {
-			os << '"' << r.stringValue() << '"';
+		if (r.isKindOf(StringClass)) {
+			os << ref_cast<String>(r);
+		} else if (r.isKindOf(NumberClass)) {
+			os << ref_cast<Number>(r);
+		} else if (r.isKindOf(PathClass)) {
+			os << ref_cast<Path>(r);
+		} else if (r.isKindOf(URLClass)) {
+			os << ref_cast<URL>(r);
+		} else if (r.isKindOf(DateClass)) {
+			os << ref_cast<Date>(r);
 		} else if (r.isKindOf(ArrayClass)) {
 			os << ref_cast<Array>(r);
-			return os;
 		} else if (r.isKindOf(OrderedSetClass)) {
 			os << ref_cast<OrderedSet>(r);
-			return os;
 		} else if (r.isKindOf(SetClass)) {
 			os << ref_cast<Set>(r);
-			return os;
 		} else if (r.isKindOf(DictionaryClass)) {
 			os << ref_cast<Dictionary>(r);
-			return os;
 		} else if (r.isKindOf(DataClass)) {
-			os << '"';
-			runtime::hexrep::dump(os, ref_cast<Data>(r).cbegin(), ref_cast<Data>(r).cend(), 256);
-			os << '"';
-			return os;
+			os << ref_cast<Data>(r);
 		} else {
 			os << r.stringValue();
 		}
@@ -55,6 +63,13 @@ namespace coconut
 	template <typename TypeT,
 		typename std::enable_if<
 			std::is_base_of<Any, TypeT>::value &&
+			!std::is_same<TypeT, String>::value &&
+			!std::is_same<TypeT, Date>::value &&
+			!std::is_same<TypeT, Data>::value &&
+			!std::is_same<TypeT, Path>::value &&
+			!std::is_same<TypeT, MutablePath>::value &&
+			!std::is_same<TypeT, URL>::value &&
+			!std::is_same<TypeT, Number>::value &&
 			!std::is_same<TypeT, Array>::value &&
 			!std::is_same<TypeT, MutableArray>::value &&
 			!std::is_same<TypeT, OrderedSet>::value &&
@@ -67,10 +82,69 @@ namespace coconut
 	>
 	inline auto operator << (std::ostream & os, ptr_declare<TypeT> const & r)
 		-> std::ostream &
-	{
-		if (r) { os << *r; }
-		return os;
-	}
+	{ if (r) { os << *r; } return os; }
+	
+	template <typename TypeT,
+		typename std::enable_if<
+			std::is_same<TypeT, String>::value ||
+			std::is_same<TypeT, Path>::value ||
+			std::is_same<TypeT, MutablePath>::value ||
+			std::is_same<TypeT, URL>::value ||
+			std::is_same<TypeT, Date>::value
+		>::type* = nullptr
+	>
+	inline auto operator << (std::ostream & os, const TypeT & r)
+		-> std::ostream &
+	{ os << '"' << r.stringValue() << '"'; return os; }
+
+	template <typename TypeT,
+		typename std::enable_if<
+			std::is_same<TypeT, String>::value ||
+			std::is_same<TypeT, Path>::value ||
+			std::is_same<TypeT, MutablePath>::value ||
+			std::is_same<TypeT, URL>::value ||
+			std::is_same<TypeT, Date>::value
+		>::type* = nullptr
+	>
+	inline auto operator << (std::ostream & os, ptr_declare<TypeT> const & r)
+		-> std::ostream &
+	{ if (r) { os << *r; } return os; }
+
+	template <typename TypeT,
+		typename std::enable_if<
+			std::is_same<TypeT, Data>::value
+		>::type* = nullptr
+	>
+	inline auto operator << (std::ostream & os, const TypeT & r)
+		-> std::ostream &
+	{ os << '"'; runtime::hexrep::dump(os, ref_cast<Data>(r).cbegin(), ref_cast<Data>(r).cend(), 256); os << '"'; return os; }
+	
+	template <typename TypeT,
+		typename std::enable_if<
+			std::is_same<TypeT, Data>::value
+		>::type* = nullptr
+	>
+	inline auto operator << (std::ostream & os, ptr_declare<TypeT> const & r)
+		-> std::ostream &
+	{ if (r) { os << *r; } return os; }
+	
+	template <typename TypeT,
+		typename std::enable_if<
+			std::is_same<TypeT, Number>::value
+		>::type* = nullptr
+	>
+	inline auto operator << (std::ostream & os, const TypeT & r)
+		-> std::ostream &
+	{ os << r.stringValue(); return os; }
+
+	template <typename TypeT,
+		typename std::enable_if<
+			std::is_same<TypeT, Number>::value
+		>::type* = nullptr
+	>
+	inline auto operator << (std::ostream & os, ptr_declare<TypeT> const & r)
+		-> std::ostream &
+	{ if (r) { os << *r; } return os; }
 	
 	template <typename TypeT,
 		typename std::enable_if<
@@ -105,10 +179,7 @@ namespace coconut
 	>
 	inline auto operator << (std::ostream & os, ptr_declare<TypeT> const & r)
 		-> std::ostream &
-	{
-		if (r) { os << *r; }
-		return os;
-	}
+	{ if (r) { os << *r; } return os; }
 
 	template <typename TypeT,
 		typename std::enable_if<
@@ -135,10 +206,7 @@ namespace coconut
 	>
 	inline auto operator << (std::ostream & os, ptr_declare<TypeT> const & r)
 		-> std::ostream &
-	{
-		if (r) { os << *r; }
-		return os;
-	}
+	{ if (r) { os << *r; } return os; }
 }
 
 #endif /* !COCONUT_RUNTIME_TO_FOUNDATION_PRINTIFIER_HPP */
